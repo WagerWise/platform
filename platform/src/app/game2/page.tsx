aee61fa49ff3033b546e3891cb555a203790e0b4
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-05-15 14:30:05 +0100

    add:game:2

--- platform/src/app/game2/page.tsx
+++ platform/src/app/game2/page.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import DashboardLayout from '../Dashboardlayout/DashboardLayout'
+import React from "react";
+import DashboardLayout from "../Dashboardlayout/DashboardLayout";
 
 const page = () => {
   return (
-<DashboardLayout>
-    GAME 2
-</DashboardLayout>
-  )
-}
+    <DashboardLayout>
+      <main>
+        <section>
+          <div className="flex items-center justify-center w-full hover:scale-80 duration-300">
+            <div className="flex items-center justify-center w-60 h-56 bg-cover  bg-no-repeat bg-[url('https://res.cloudinary.com/dtfvdjvyr/image/upload/v1715775396/amico_pctqsg.png')] bg-center hover:border-primary duration-300 "></div>
+          </div>
+        </section>
+      </main>
+    </DashboardLayout>
+  );
+};
 
-export default page
+export default page;
--- platform/tailwind.config.ts
+++ platform/tailwind.config.ts
@@ -8,10 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#FFD700",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "card":
+          "url('https://res.cloudinary.com/dtfvdjvyr/image/upload/v1715774271/amico_drhzdg.png')",
       },
     },
   },
