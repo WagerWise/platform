0c5d69545bf81ef27090d8bad740b90c179afcc9
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-05-24 12:39:04 +0100

    add:game

---
+++ platform/src/app/api/transfer.js
@@ -0,0 +1,11 @@
+import {
+  Connection,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+export default async (req, res) => {};
